287459d4ec7d1c811671a7550b9a84a99c05c2c0
deshraj.thakur <deshrajnitbhopal.com>
2020-07-08 08:37:50 +0530

    deshraj|spring client poc

--- spring-client/src/main/java/raj/com/springclient/controller/ClientController.java
+++ spring-client/src/main/java/raj/com/springclient/controller/ClientController.java
@@ -1,19 +1,34 @@
 package raj.com.springclient.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
+@RequestMapping("/")
+@PropertySource("application.properties")
 public class ClientController {
 
 
-    @Value("${massage : default defaultMessageVal}")
+    @Value("${message}")
     private String message;
+    @Value("${language}")
+    private String language;
 
-    @GetMapping("/message")
+    @GetMapping("/message-test")
     public String getMessage(){
         return message;
+    }
+
+    @GetMapping("/language-test")
+    public String getLanguage(){
+        return language;
     }
 
 }
