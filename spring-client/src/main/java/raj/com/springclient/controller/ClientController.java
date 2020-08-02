9be99a535c77b7c7d8856b3a5a94c49a9fc5459e
deshraj.thakur <deshrajnitbhopal.com>
2020-08-02 22:37:22 +0530

    deshraj|config client

--- spring-client/src/main/java/raj/com/springclient/controller/ClientController.java
+++ spring-client/src/main/java/raj/com/springclient/controller/ClientController.java
@@ -1,18 +1,16 @@
 package raj.com.springclient.controller;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @RestController
 @RequestMapping("/")
 @PropertySource("application.properties")
+@RefreshScope
 public class ClientController {
 
 
